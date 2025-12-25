bc7dce37b5b55b534f4e56ec30324525f91df28f
halloween <127612472+domainthefesh@users.noreply.github.com>
2025-12-26 02:12:15 +0800

    音乐系统初步建立
    1.添加了一个AudioManager的文件，现在你可以通过这个文件的函数来添加resource里面的音乐了（一定要是最标准的wav格式！！！）
    2.增加了Loading界面结束音效
    3.增加了主界面bgm
    4.更新了readme里的分工部分

---
+++ Classes/AudioManager.h
@@ -0,0 +1,50 @@
+#ifndef AUDIO_MANAGER_H
+#define AUDIO_MANAGER_H
+
+#include "cocos2d.h"
+#include "SimpleAudioEngine.h"
+
+USING_NS_CC;
+using namespace CocosDenshion;
+
+class AudioManager {
+public:
+	//单例
+	static AudioManager* getInstance();
+
+	//初始化音频
+	void initAudio();
+
+	//循环播放背景音乐
+	void playBgMusic(const std::string& musicPath);
+
+	//暂停背景音乐
+	void pauseBgMusic();
+
+	//停止背景音乐
+	void stopBgMusic();
+
+	//播放音效
+	void playEffect(const std::string& effectPath);
+
+	//设置背景音乐音量(0~1)
+	void setBgVolume(float Volume);
+
+	//设置音效音乐音量
+	void setEffectVolume(float Volume);
+
+private:
+	//私有构造，禁止外部修改
+	AudioManager() = default;
+	~AudioManager() = default;
+
+	//禁止拷贝
+	AudioManager(const AudioManager&) = delete;
+	AudioManager& operator=(const AudioManager&) = delete;
+
+	static AudioManager* _instance;
+};
+
+
+
+#endif // AUDIO_MANAGER_H
